e17ae38845974b005aaeaa31817aad06efa32538
Weston Bodily <westonbodily7z@gmail.com>
2018-05-02 12:13:29 -0600

    kivy mockup progress main

--- kivyMockup/main.py
+++ kivyMockup/main.py
@@ -35,6 +35,47 @@ class PongPaddle(Widget):
             ball.center_x = self.center_x + (mx * 50)
 
 
+class LinkImage(Image):
+    source="./sprites/linkDown1.png"
+
+    def move(self, ball):
+        self.center_x = ball.center_x
+        self.center_y = ball.center_y
+
+    pass
+
+class Background20(Image):
+    source="./sprites/background20.png"
+    pass
+
+class Background21(Image):
+    source="./sprites/background21.png"
+    pass
+
+class Background10(Image):
+    source="./sprites/background10.png"
+    pass
+
+class Background11(Image):
+    source="./sprites/background11.png"
+    pass
+
+class Background00(Image):
+    source="./sprites/background00.png"
+    pass
+
+class Background01(Image):
+    source="./sprites/background01.png"
+    pass
+
+class Background30(Image):
+    source="./sprites/background30.png"
+    pass
+
+class Background31(Image):
+    source="./sprites/background31.png"
+    pass
+
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
@@ -65,6 +106,53 @@ class PongGame(Widget):
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self._keyboard.bind(on_key_up=self._on_keyboard_up)
 
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def seedUpdate(self):
+        self.seedText = str(self.ball.center_x), str(self.ball.center_y), str(self.mapIndy), str(self.mapIndx*2)
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'w':
+            self.ball.velocity_y = 10
+        if keycode[1] == 's':
+            self.ball.velocity_y = -10
+        if keycode[1] == 'a':
+            self.ball.velocity_x = -10
+        if keycode[1] == 'd':
+            self.ball.velocity_x = 10
+        if keycode[1] == 'up':
+            self.ball.velocity_y = 10
+        if keycode[1] == 'down':
+            self.ball.velocity_y = -10
+        if keycode[1] == 'left':
+            self.ball.velocity_x = -10
+        if keycode[1] == 'right':
+            self.ball.velocity_x = 10
+        if keycode[1] == 'm':
+            self.seedUpdate()
+        return True
+
+    def _on_keyboard_up(self, keyboard, keycode):
+        if keycode[1] == 'w':
+            self.ball.velocity_y = 0
+        if keycode[1] == 's':
+            self.ball.velocity_y = 0
+        if keycode[1] == 'a':
+            self.ball.velocity_x = 0
+        if keycode[1] == 'd':
+            self.ball.velocity_x = 0
+        if keycode[1] == 'up':
+            self.ball.velocity_y = 0
+        if keycode[1] == 'down':
+            self.ball.velocity_y = 0
+        if keycode[1] == 'left':
+            self.ball.velocity_x = 0
+        if keycode[1] == 'right':
+            self.ball.velocity_x = 0
+        return True
+
     def serve_ball(self, vel=(0, 0)):
         self.seedText = ""
         self.ball.center = self.center
@@ -76,6 +164,50 @@ class PongGame(Widget):
         self.bkgr10.center_x=7500 
         self.bkgr30.center_x=7500 
         self.bkgr31.center_x=7500 
+
+
+    def travel(self):
+        if(self.mapIndx==0 and self.mapIndy==0):   
+            self.bkgr00.center_x=800 
+            self.bkgr01.center_x=7500 
+            self.bkgr10.center_x=7500
+        elif(self.mapIndx==0 and self.mapIndy==1):   
+            self.bkgr01.center_x=800 
+            self.bkgr00.center_x=7500 
+            self.bkgr11.center_x=7500
+        elif(self.mapIndx==1 and self.mapIndy==0):   
+            self.bkgr10.center_x=800
+            self.bkgr00.center_x=7500 
+            self.bkgr01.center_x=7500 
+            self.bkgr11.center_x=7500 
+            self.bkgr20.center_x=7500
+            self.bkgr21.center_x=7500
+            self.bkgr30.center_x=7500
+            self.bkgr31.center_x=7500
+        elif(self.mapIndx==1 and self.mapIndy==1):   
+            self.bkgr11.center_x=800
+            self.bkgr01.center_x=7500 
+            self.bkgr10.center_x=7500 
+            self.bkgr21.center_x=7500
+        elif(self.mapIndx==2 and self.mapIndy==0):   
+            self.bkgr20.center_x=800
+            self.bkgr10.center_x=7500 
+            self.bkgr21.center_x=7500 
+            self.bkgr30.center_x=7500
+        elif(self.mapIndx==2 and self.mapIndy==1):   
+            self.bkgr21.center_x=800
+            self.bkgr11.center_x=7500 
+            self.bkgr20.center_x=7500 
+            self.bkgr31.center_x=7500
+        elif(self.mapIndx==3 and self.mapIndy==0):   
+            self.bkgr30.center_x=800
+            self.bkgr20.center_x=7500 
+            self.bkgr31.center_x=7500 
+            self.bkgr21.center_x=7500
+        elif(self.mapIndx==3 and self.mapIndy==1):   
+            self.bkgr31.center_x=800
+            self.bkgr21.center_x=7500 
+            self.bkgr30.center_x=7500 
 
     def update(self, dt):
         self.ball.move()
