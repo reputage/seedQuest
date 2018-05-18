d18e15081a24a08f6ca16ab2d2e5a72aa28f87cc
Weston Bodily <westonbodily7z@gmail.com>
2018-05-18 16:51:41 -0600

    Git LFS stress test

---
+++ Unity/SeedUnity/Assets/Scripts/FixedFollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedFollowCamera : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+
+	}
+}
---
+++ Unity/SeedUnity/Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour {
+
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene("procedural");
+    }
+}
