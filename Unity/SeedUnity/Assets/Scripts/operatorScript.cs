93c4f8c8d5e54a3982c36756b6d3c1d5570a80c8
Weston Bodily <westonbodily7z@gmail.com>
2018-05-21 11:49:22 -0600

    Added UI elements

---
+++ Unity/SeedUnity/Assets/Scripts/operatorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class operatorScript : MonoBehaviour {
+
+    // Use this for initialization
+
+    //public GameObject defaultButton;
+    //public GameObject seedButton;
+    //public GameObject generateButton;
+    static string seedWord;
+
+	void Start () {
+		
+	}
+	
+    public void defaultClick(){
+        Debug.Log("Default button clicked!");
+    }
+
+    public void seedClick()
+    {
+        Debug.Log("Seed button clicked!");
+    }
+
+    public void generateClick()
+    {
+        Debug.Log("Generate button clicked!");
+    }
+
+    public void GetInput(string seedInput) {
+        Debug.Log("Input: " + seedInput);
+        seedWord = seedInput;
+        Debug.Log("Word: " + seedWord);
+    }
+}
