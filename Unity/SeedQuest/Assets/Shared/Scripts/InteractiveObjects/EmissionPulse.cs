a7ea30ede88ee3c7c4e776c191466f3224e7e293
Michael Mendoza <m.mendoza711@gmail.com>
2018-06-11 10:36:38 +0100

    Added SeedQuest unity project

---
+++ Unity/SeedQuest/Assets/Shared/Scripts/InteractiveObjects/EmissionPulse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionPulse : MonoBehaviour {
+
+    public float maxLevel = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Renderer rend = GetComponent<Renderer>();
+        Material mat = rend.material;
+
+        float intensity = Mathf.PingPong(Time.time, 1.0f);
+        Color baseColor = new Color(maxLevel, maxLevel, maxLevel); //Replace this with whatever you want for your base color at emission level '1'
+        Color finalColor = baseColor * Mathf.LinearToGammaSpace(intensity);
+        mat.SetColor("_EmissionColor", finalColor);
+	}
+}
