3b060e47218c6945e3a7c34a174972bac9c957cb
Michael Mendoza <m.mendoza711@gmail.com>
2018-07-03 18:27:59 -0600

    Created Pathfinding Scene

---
+++ Unity/SeedAssets/Assets/_Scenes/PathFinding/Grid.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    public Transform player;
+    public LayerMask unwalkableMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    Node[,] grid;
+    
+	float nodeDiameter;
+	int gridSizeX, gridSizeY;
+    
+	void Start() {
+		nodeDiameter = nodeRadius * 2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+		CreateGrid();
+	}
+
+	void CreateGrid() { 
+		grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++) 
+            for (int y = 0; y < gridSizeY; y++) {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+        }
+	} 
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition) {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+
+        if(grid != null) {
+            Node playerNode = NodeFromWorldPoint(player.position);
+
+            foreach(Node n in grid) {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if(playerNode == n) {
+                    Gizmos.color = Color.cyan;
+                }
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
+    }
+}
---
+++ Unity/SeedAssets/Assets/_Scenes/PathFinding/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node  {
+
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool _walkable, Vector3 _wPos) {
+        walkable = _walkable;
+        worldPosition = _wPos;
+    }
+}
