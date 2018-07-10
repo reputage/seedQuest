fd85cccd84da2b6ecf7cfae7298c81a83aea7346
Michael Mendoza <m.mendoza711@gmail.com>
2018-07-10 17:54:46 -0600

    Added Campfire with Particle Efects. Added PathFinder.

---
+++ Unity/SeedAssets/Assets/_Scenes/PathFinding/PathFinder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+
+    public Transform target;
+    public LineRenderer line;
+
+    Pathfinding pathfinding;
+    Vector3[] path;
+
+    private void Start()
+    {
+        //Vector3[] waypoints = FindPath(transform.position, target.position);
+        //if (waypoints.Length > 0)
+        //    DrawPath();
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSucessful)
+    {
+        if (pathSucessful)
+        {
+            path = newPath;
+            DrawPath();
+        }
+    }
+
+    void DrawPath()
+    {
+        line.positionCount = path.Length;
+        line.SetPositions(path);
+    }
+}
