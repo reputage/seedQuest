9da9945450c28294799f976220bc6eacb704ff61
Weston Bodily <westonbodily7z@gmail.com>
2018-08-10 16:36:48 -0600

    adding sound to animation in progress...

---
+++ Unity/SeedQuest/Assets/Shared/Scripts/Player/footstepSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footstepSound : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip footstep1;
+
+	void Start () 
+    {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+		
+	}
+
+    void playAudio(AudioClip sound)
+    {
+        Debug.Log("Footstep sound playing...");
+        audioSource.PlayOneShot(footstep1);
+    }
+}
