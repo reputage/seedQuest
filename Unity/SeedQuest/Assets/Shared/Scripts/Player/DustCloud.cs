514099f2ca103d67131e323148b9b07566695ab0
Weston Bodily <westonbodily7z@gmail.com>
2018-08-21 10:13:01 -0600

    cloud movement script in progress

--- Unity/SeedQuest/Assets/Shared/Scripts/Player/DustCloud.cs
+++ Unity/SeedQuest/Assets/Shared/Scripts/Player/DustCloud.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class DustCloud : MonoBehaviour {
 
-    public float verticalSpeed;
+    public Vector3 velocity;
+    public Vector3 start;
+    public Vector3 rotate;
 
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+    {
+        // Temporary, for testing:
+        initialize(new Vector3(1, 1, 1), new Vector3(-24, 0, 5), new Vector3(1, 1, 1));
+
+        transform.position = start;	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+
+    void Update () 
+    {
+        transform.position += velocity * Time.fixedDeltaTime; // not sure if I need the time var here
+        transform.Rotate(rotate * Time.fixedDeltaTime);
 	}
+
+
+    void initialize(Vector3 vel, Vector3 st, Vector3 rot)
+    {
+        velocity = vel;
+        start = st;
+        rotate = rot;
+    }
 }
---
+++ Unity/SeedQuest/Assets/Shared/Scripts/Player/cloudGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudGenerator : MonoBehaviour {
+
+    public int cloudIter;
+
+	// Use this for initialization
+	void Start () {
+        cloudIter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        cloudIter += 1;
+		
+	}
+}
