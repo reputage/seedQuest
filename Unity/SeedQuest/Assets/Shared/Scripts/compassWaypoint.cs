cd04c5ed664f6acc7c23e5cc5d260d184c721808
Weston Bodily <westonbodily7z@gmail.com>
2018-08-24 16:13:28 -0600

    code is commented, UI needs to be fine-tuned

--- Unity/SeedQuest/Assets/Shared/Scripts/compassWaypoint.cs
+++ Unity/SeedQuest/Assets/Shared/Scripts/compassWaypoint.cs
@@ -22,19 +22,32 @@ public class compassWaypoint : MonoBehaviour {
     // if the angle is > some amount (60 degrees?) then don't update the position
 
 
-	// Use this for initialization
 	void Start ()
     {
+        initialize();
+    }
+	
+
+	void Update () 
+    {
+        updateWaypoint();
+	}
+
+    // Set the parameters for xRange, yHeight, angleRange, and find necessary components
+    void initialize()
+    {
         forward = transform.forward;
-        xRange = 100;
+        rPosition = GetComponent<RectTransform>();
+
+        xRange = 45;
         yHeight = 170;
         angleRange = 50;
+    }
 
-        rPosition = GetComponent<RectTransform>();
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    // This calculates the angles for direction the player is facing, 
+    //  angle between the player and the target, and sets the 
+    //  marker's X position based on both angles.
+    void updateWaypoint()
     {
         targetDir = target.transform.position - player.transform.position;
 
@@ -42,46 +55,44 @@ public class compassWaypoint : MonoBehaviour {
         float playerAngle = player.transform.eulerAngles.y;
 
         // Don't ask me why this is necessary, it just is.
-
         if (playerAngle > 180)
         {
             playerAngle -= 360; //  playerAngle;
         }
 
         // Don't ask me why this is necessary either. 
-        // Just don't change it unless you know what you're doing.
+        // Just don't change it unless you know what you're doing. 
         float angleDiff = angle - playerAngle;
         if (angleDiff > 180)
-        {
             angleDiff -= 360;
-        }
         if (angleDiff < -180)
-        {
             angleDiff += 360;
-        }
+       
+        //Debug.Log("angleDiff: " + angleDiff + " angle: " + angle + " playerAngle: " + playerAngle);
 
-        Debug.Log("angleDiff: " + angleDiff + " angle: " + angle + " playerAngle: " + playerAngle);
-
-
-        if (angleDiff > -50)
+        if (angleDiff > - angleRange)
         {
-            float newX = (angleDiff);
-            Debug.Log(newX);
+            float newX = (angleDiff / angleRange) * xRange;
             reposition(newX);
         }
-        else if(angleDiff < 50)
+        else if (angleDiff < angleRange)
         {
-            float newX = (angleDiff);
-            Debug.Log(newX);
+            float newX = (angleDiff / angleRange) * xRange;
             reposition(newX);
         }
+    }
 
-
-	}
-
+    // Repositions the marker. This is called in the above function.
     void reposition(float newX)
     {
         rPosition.anchoredPosition = new Vector3(newX, yHeight, 0);
     }
+
+    // Sets a new target for the compass waypoint to track. Needs to be tested.
+    void setTarget(GameObject newTarget)
+    {
+        target = newTarget;
+    }
+
 }
 
