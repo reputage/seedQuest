f10d98c9b20372a1d2c33bcf7c4851ea1b01e79e
Weston Bodily <westonbodily7z@gmail.com>
2018-08-31 15:54:38 -0600

    waypoint marker for the minimap is functional

--- Unity/SeedQuest/Assets/Shared/Scripts/Compass_UI/MinimapObjects.cs
+++ Unity/SeedQuest/Assets/Shared/Scripts/Compass_UI/MinimapObjects.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MinimapObjects : MonoBehaviour {
 
     public GameObject childObjects;
+    public Interactable target;
+    public RectTransform waypoint;
+    public Transform player;
+
     public GameStateData gameState;
 
     private bool checking;
@@ -13,6 +18,7 @@ public class MinimapObjects : MonoBehaviour {
     {
         deactivate();
         checking = true;
+
     }
 
     void Update()
@@ -29,6 +35,9 @@ public class MinimapObjects : MonoBehaviour {
                 deactivate();
             }
         }
+
+        target = gameState.currentAction;
+        calculateWaypointPosition();
     }
 
     void deactivate()
@@ -39,6 +48,39 @@ public class MinimapObjects : MonoBehaviour {
     void activate()
     {
         childObjects.SetActive(true);
+    }
+
+
+    void calculateWaypointPosition()
+    {
+        Vector3 playerNoY = player.position;
+        Vector3 targetNoY = target.transform.position;
+
+        playerNoY.y = 0;
+        targetNoY.y = 0;
+
+        // I'm not sure why, but the waypoint won't be in the right spot unless
+        //  this calculation is performed. Apparently the scaling for the
+        //  x-axis and z-axis are different, so it may be a resolution problem?
+
+        playerNoY.z = playerNoY.z / 25 * 15;
+        targetNoY.z = targetNoY.z / 25 * 15;
+
+        Vector3 diff = targetNoY - playerNoY;
+        float normDist = Vector3.Distance(player.position, target.transform.position);
+        float dist = Vector3.Distance(targetNoY, playerNoY);
+
+        Debug.Log("Dist: " + dist + " normDist: " + normDist + " diff: " + diff);
+
+        if (dist < 15)
+        {
+            waypoint.anchoredPosition = new Vector3(diff.x / 15 * 120, diff.z / 15 * 120, 0);
+
+        }
+        else
+        {
+            waypoint.anchoredPosition = new Vector3(diff.x / dist * 120, diff.z / (dist) * 120, 0);   
+        }
     }
 
 }
