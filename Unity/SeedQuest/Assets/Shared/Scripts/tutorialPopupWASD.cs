673155468b6eb8a560c2ff32ab2fbf8703c0e5a3
Weston Bodily <westonbodily7z@gmail.com>
2018-09-04 12:54:54 -0600

    prefabs made for compass and minimap, tutorial popups in progress

---
+++ Unity/SeedQuest/Assets/Shared/Scripts/tutorialPopupWASD.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialPopupWASD : MonoBehaviour {
+
+
+
+
+    void Start () 
+    {
+		
+	}
+	
+
+    void Update () 
+    {
+		
+	}
+}
