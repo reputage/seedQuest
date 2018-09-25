bb28fec600d80b754038302fed936d4405f5a3a9
Weston Bodily <westonbodily7z@gmail.com>
2018-09-25 13:58:50 -0600

    code cleanup

--- Unity/SeedQuest/Assets/Shared/Scripts/Camera/MinimapCamera.cs
+++ Unity/SeedQuest/Assets/Shared/Scripts/Camera/MinimapCamera.cs
@@ -17,7 +17,11 @@ public class MinimapCamera : MonoBehaviour {
 	void Start () 
     {
         cameraOffset.y = 100;
-        initializeRes();
+        screenShotRender.enabled = false;
+
+        // Comment this line out if you want to just use the camera, 
+        //  and not the rendered map image
+        initializeImage();
 	}
 	
 
@@ -27,14 +31,19 @@ public class MinimapCamera : MonoBehaviour {
         transform.position += cameraOffset;
 	}
 
-    void initializeRes()
+    // Render an image of the map by having the camera take a "screenshot"
+    //  of the world from overhead, then assigning that screenshot
+    //  to a UI image which will move itself.
+    void initializeImage()
     {
         Camera cameraRef = GetComponent<Camera>();
         cameraRef.transform.position = new Vector3(0, 500, 0);
+
         RenderTexture other = cameraRef.targetTexture;
         RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
         cameraRef.targetTexture = rt;
         RenderTexture.active = rt;
+
         Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
         cameraRef.Render();
         screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
@@ -44,6 +53,7 @@ public class MinimapCamera : MonoBehaviour {
 
         cameraRef.targetTexture = null;
         RenderTexture.active = null;
+        screenShotRender.enabled = true;
 
         cameraRef.targetTexture = other;
         cameraRef.enabled = false;
--- Unity/SeedQuest/Assets/Shared/Scripts/CompassUI/minimap.cs
+++ Unity/SeedQuest/Assets/Shared/Scripts/CompassUI/minimap.cs
@@ -5,23 +5,16 @@ using UnityEngine;
 public class minimap : MonoBehaviour
 {
 
-    //public Transform player = PlayerManager.Transform;
-
     public float yScale;
     public float xScale;
 
     public int xOffset;
     public int yOffset;
 
-    // Use this for initialization
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        // Move the image around to simulate the camera following the player.
         transform.position = new Vector3(-(PlayerManager.Transform.position.x * xScale) + xOffset, -(PlayerManager.Transform.position.z * yScale) + yOffset, 0);
     }
 }
