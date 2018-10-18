198d59969a7222673cc9edff879ccd1763aff122
Weston Bodily <westonbodily7z@gmail.com>
2018-10-18 13:40:07 -0600

    Get request function working with didery

---
+++ Unity/SeedQuest/Assets/Shared/Scripts/Seeds/DideryInterface.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class DideryInterface{
+
+
+
+    public static IEnumerator GetRequest(string uri)
+    {
+        UnityWebRequest uwr = UnityWebRequest.Get(uri);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
+
+    // Needs testing
+    public static IEnumerator PostRequest(string url, string json)
+    {
+        var uwr = new UnityWebRequest(url, "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
+
+    // Needs testing
+    public static IEnumerator PutRequest(string url)
+    {
+        byte[] dataToPut = System.Text.Encoding.UTF8.GetBytes("Hello, This is a test");
+        UnityWebRequest uwr = UnityWebRequest.Put(url, dataToPut);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
+
+    // Needs testing
+    public static IEnumerator DeleteRequest(string url)
+    {
+        UnityWebRequest uwr = UnityWebRequest.Delete(url);
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Deleted");
+        }
+    }
+}
---
+++ Unity/SeedQuest/Assets/Shared/Scripts/Seeds/dideryButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dideryButton : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    public void dideryGetTest() 
+    {
+        StartCoroutine(DideryInterface.GetRequest("http://localhost:8080/blob/"));
+    }
+
+}
