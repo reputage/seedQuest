19d6f5d96bb4fa364871526c591e080f73a35dc0
Michael Mendoza <m.mendoza711@gmail.com>
2019-01-11 13:56:22 -0700

    For Sandbox: Interactable's label position can be changed in real time and label can be selected to show options.

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/InteractableUI.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/InteractableUI.cs
@@ -17,71 +17,85 @@ public class InteractableUI {
     public Vector3 rotationOffset = new Vector3(0, 0, 0);
     public Vector3 positionOffset = new Vector3(0, 0, 0);
 
+    private Interactable parent;
     private GameObject actionUI = null;
     private Button[] actionButtons;
 
+    /// <summary> Initialize Interactable UI with Prompt Text and Buttons </summary>
+    /// <param name="interactable">Parent Interactable Object</param>
     public void Initialize(Interactable interactable) {
-        Vector3 positionOffset = Vector3.zero;
-        if (interactable.stateData != null)
-            positionOffset = interactable.stateData.labelPosOffset;
-        Vector3 position = interactable.transform.position + positionOffset;
-        Quaternion rotate = Quaternion.identity;
+        parent = interactable;
 
         int modeIndex = 0;
         modeIndex = mode == InteractableUIMode.GridSelect ? 1 : modeIndex;
         modeIndex = mode == InteractableUIMode.ListSelect ? 2 : modeIndex;
 
-        actionUI = GameObject.Instantiate(InteractableManager.Instance.actionSpotIcons[modeIndex], position, rotate, InteractableManager.Instance.transform);
+        actionUI = GameObject.Instantiate(InteractableManager.Instance.actionSpotIcons[modeIndex], InteractableManager.Instance.transform);
         SetScale();
-        SetPositionOffset();
-
-        var text = actionUI.GetComponentInChildren<TMPro.TextMeshProUGUI>();
-        text.fontSize = fontSize; 
-
-        if (interactable.stateData != null)
-            text.text = interactable.stateData.interactableName;
-        else
-            text.text = "Error: Missing StateData";
-        
-        actionButtons = actionUI.GetComponentsInChildren<Button>();
-        if (mode == InteractableUIMode.NextPrevSelect) {
-            actionButtons[0].onClick.AddListener(interactable.NextAction);
-            actionButtons[1].onClick.AddListener(interactable.PrevAction);
-        }
-        else if (mode == InteractableUIMode.GridSelect || mode == InteractableUIMode.ListSelect) {
-            for (int i = 0; i < 4; i++) {
-                var actionText = actionButtons[i].GetComponentInChildren<TMPro.TextMeshProUGUI>();
-                actionText.text = interactable.stateData.getStateName(i);
-            }
-
-            actionButtons[0].onClick.AddListener(delegate { interactable.DoAction(0); });
-            actionButtons[1].onClick.AddListener(delegate { interactable.DoAction(1); });
-            actionButtons[2].onClick.AddListener(delegate { interactable.DoAction(2); });
-            actionButtons[3].onClick.AddListener(delegate { interactable.DoAction(3); });
-        }
-
-        hideActions();
-
-        // Create Triggers for HoverEvents
-        foreach (Button button in actionButtons) {
-            setButtonHoverEvents(button);
-        }
-    }
+        SetPosition();
+        SetupLabelButton();
+        SetupActionButtons();
+    } 
 
     public void Update() {
         if(isReady()) {
-            if(useRotateToCamera) {
-                BillboardInteractable();
-                SetRotationOffset();
-            }
-            else {
-                SetRotation(); 
-            }
+            SetPosition();
+            SetRotation();
         }
     }
 
     public bool isReady() {
         return actionUI != null;
+    }
+
+    public void SetupLabelButton()
+    {
+        var textList = actionUI.GetComponentsInChildren<TMPro.TextMeshProUGUI>();
+        if (parent.stateData != null)
+            textList[0].text = parent.stateData.interactableName;
+        else
+            textList[0].text = "Error: Missing StateData";
+
+        Button[] buttons = actionUI.GetComponentsInChildren<Button>();
+        buttons[0].onClick.AddListener(showActions);
+    }
+
+    public void SetupActionButtons()
+    {
+        var textList = actionUI.GetComponentsInChildren<TMPro.TextMeshProUGUI>();
+        foreach (TMPro.TextMeshProUGUI text in textList)
+            text.fontSize = fontSize;
+
+        Button[] buttons = actionUI.GetComponentsInChildren<Button>();
+        actionButtons = new Button[buttons.Length - 1];
+        System.Array.Copy(buttons, 1, actionButtons, 0, actionButtons.Length);
+
+        if (mode == InteractableUIMode.NextPrevSelect)
+        {
+            actionButtons[0].onClick.AddListener(parent.NextAction);
+            actionButtons[1].onClick.AddListener(parent.PrevAction);
+        }
+        else if (mode == InteractableUIMode.GridSelect || mode == InteractableUIMode.ListSelect)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                var actionText = actionButtons[i].GetComponentInChildren<TMPro.TextMeshProUGUI>();
+                actionText.text = parent.stateData.getStateName(i);
+            }
+
+            actionButtons[0].onClick.AddListener(delegate { parent.DoAction(0); });
+            actionButtons[1].onClick.AddListener(delegate { parent.DoAction(1); });
+            actionButtons[2].onClick.AddListener(delegate { parent.DoAction(2); });
+            actionButtons[3].onClick.AddListener(delegate { parent.DoAction(3); });
+        }
+
+        hideActions();
+
+        // Create Triggers for HoverEvents
+        foreach (Button button in actionButtons)
+        {
+            setButtonHoverEvents(button);
+        }
     }
 
     // Create TriggerEntry and add callback
@@ -112,30 +126,34 @@ public class InteractableUI {
         }
     }
 
-    public void BillboardInteractable()
-    {
+    public void SetScale() {
+        actionUI.GetComponent<RectTransform>().localScale = new Vector3(-0.01f * scaleSize, 0.01f * scaleSize, 0.01f * scaleSize);
+    }
+
+    public void SetPosition() {
+        Vector3 labelPositionOffset = Vector3.zero;
+        if (parent.stateData != null) labelPositionOffset = parent.stateData.labelPosOffset;
+        Vector3 position = parent.transform.position + labelPositionOffset + positionOffset;
+        actionUI.GetComponent<RectTransform>().position = position;
+    }
+
+    public void SetRotation() {
+        if (useRotateToCamera) {
+            BillboardInteractable();
+            actionUI.GetComponent<RectTransform>().Rotate(rotationOffset);
+        }
+        else {
+            actionUI.GetComponent<RectTransform>().rotation = Quaternion.Euler(rotationOffset);
+        }
+    }
+
+    public void BillboardInteractable() {
         Vector3 targetPosition = Camera.main.transform.position;
         Vector3 interactablePosition = actionUI.transform.position;
         Vector3 lookAtDir = targetPosition - interactablePosition;
 
         Quaternion rotate = Quaternion.LookRotation(lookAtDir);
         actionUI.transform.rotation = rotate;
-    }
-
-    public void SetScale() {
-        actionUI.GetComponent<RectTransform>().localScale = new Vector3(-0.01f * scaleSize, 0.01f * scaleSize, 0.01f * scaleSize);
-    }
-
-    public void SetRotation() {
-        actionUI.GetComponent<RectTransform>().rotation = Quaternion.Euler(rotationOffset);
-    }
-
-    public void SetRotationOffset() {
-        actionUI.GetComponent<RectTransform>().Rotate(rotationOffset);
-    }
-
-    public void SetPositionOffset() {
-        actionUI.GetComponent<RectTransform>().Translate(positionOffset);
     }
 
     public void SetText(string text) {
