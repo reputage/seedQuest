3ea35565c5e4a8dd91cf3ff3c489bca3356bd517
Michael Mendoza <m.mendoza711@gmail.com>
2019-01-17 14:03:44 -0700

    Sandbox: Added SurveyData

---
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/SurveyData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SurveyDataItem {
+    public string question;
+    public string answer;
+}
+
+[CreateAssetMenu(menuName = "Survey/SurveyData")]
+public class SurveyData : ScriptableObject {
+
+    public List<SurveyDataItem> surveyData;
+
+} 
