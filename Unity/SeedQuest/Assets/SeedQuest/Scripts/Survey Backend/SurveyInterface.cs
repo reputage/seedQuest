d8ddd4b6fa2a69079b49d2d903aee4cd145e8d15
Weston Bodily <westonbodily7z@gmail.com>
2019-01-18 12:49:40 -0700

    Survey backend scripts in progress

---
+++ Unity/SeedQuest/Assets/SeedQuest/Scripts/Survey Backend/SurveyInterface.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public static class SurveyInterface 
+{
+    // Send Post request to survey API
+    public static IEnumerator PostRequest(string url, string apiHeader, string apiToken, string surveyId, byte[] csvFile)
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("surveyId", surveyId));
+        formData.Add(new MultipartFormFileSection("file:", csvFile));
+        formData.Add(new MultipartFormDataSection("content-type", "text/csv"));
+
+        UnityWebRequest uwr = UnityWebRequest.Post(url, formData);
+        uwr.SetRequestHeader(apiHeader, apiToken);
+
+        yield return uwr.SendWebRequest();
+
+        if (uwr.isNetworkError || uwr.isHttpError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        else
+        {
+            Debug.Log("Received: " + uwr.downloadHandler.text);
+        }
+    }
+
+}
---
+++ Unity/SeedQuest/Assets/SeedQuest/Scripts/Survey Backend/SurveyTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurveyTests : MonoBehaviour {
+
+    private string apiHeader = "X-API-TOKEN";
+    private string apiToken = "1234";
+    private string expectsHeader = "expects:";
+    private string expectsData = "1234";
+    private string url = "http/stuff";
+    private string surveyId = "surveyID12345";
+    private byte[] fileBytes;
+
+    SurveyDataHandler surveyDataHandler = new SurveyDataHandler();
+
+    void runTests()
+    {
+        testPostRequest();
+    }
+
+    void testPostRequest()
+    {
+        surveyDataHandler.postRequest(url, apiHeader, apiToken, surveyId, fileBytes);
+    }
+
+}
