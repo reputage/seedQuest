86ead9af568bf04a1a27c4ee83234bc9aa60b231
Weston Bodily <westonbodily7z@gmail.com>
2019-01-22 08:39:56 -0700

    minor changes

--- Unity/SeedQuest/Assets/SeedQuest/Scripts/Tests/SurveyTests.cs
+++ Unity/SeedQuest/Assets/SeedQuest/Scripts/Tests/SurveyTests.cs
@@ -6,18 +6,13 @@ using UnityEngine;
 public class SurveyTests : MonoBehaviour {
 
     public string apiHeader = "X-API-TOKEN";
-    public string apiToken = "2dDzDzOcKy5muXvFITA6dvyz9nvtmeefks8fgKpu";
+    public string apiToken = "...";
     public string expectsHeader = "Expects:";
     public string expectsData = "1234";
-    public string url = "https://co1.qualtrics.com/API/v3/responseimports";
-    public string surveyId = "SV_eWZOGc8YXsdaqTb";
+    public string url = "https://...";
+    public string surveyId = "...";
     public string testCsv = "Question1,answer1";
     public byte[] fileBytes;
-
-    //'X-API-TOKEN: 2dDzDzOcKy5muXvFITA6dvyz9nvtmeefks8fgKpu' -H "Expects:" -F 'surveyId=SV_eWZOGc8YXsdaqTb' 
-    // -F 'file=@testSurveyResponse.csv;type=text/csv' 'https://co1.qualtrics.com/API/v3/responseimports'
-
-    //SurveyDataHandler surveyDataHandler = new SurveyDataHandler();
 
     public void runTests()
     {
