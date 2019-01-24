f931804e582dc8f5474533b2269c9fd32d56021d
Weston Bodily <westonbodily7z@gmail.com>
2019-01-24 08:37:40 -0700

    survey API testing

---
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/SurveyInterface/SurveyMonkeyTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurveyMonkeyTest : MonoBehaviour {
+
+
+	private void Start()
+	{
+        sendRequestData();
+	}
+
+	public void sendRequestData()
+    {
+        Debug.Log("Starting Request.");
+        StartCoroutine(HttpTest.postRequest());
+        Debug.Log("Request Finished.");
+    }
+
+}
