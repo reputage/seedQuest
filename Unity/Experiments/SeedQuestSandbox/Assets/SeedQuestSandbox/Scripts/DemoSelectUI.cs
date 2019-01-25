ff1ebc8874570f50a33e2aff9a06844c774e0094
Michael Mendoza <m.mendoza711@gmail.com>
2019-01-25 16:14:15 -0700

    Sandbox: Added DemoSelect Scene

---
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/DemoSelectUI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+[System.Serializable]
+public class DemoInfo {
+    public string name;
+    public string sceneName;
+    public string demoTitle;
+    public string demoText;
+    public Sprite demoImage;
+}
+
+public class DemoSelectUI : MonoBehaviour {
+
+    public DemoInfo[] demoList;
+    public DemoInfo selectedDemo;
+    public GameObject selectButtonPrefab;
+    public Vector3 buttonOffset = new Vector3(170, 250, 0);
+    public int buttonPadding = 60;
+
+    private void Start() {
+
+        selectedDemo = demoList[0];
+        GameObject sideNav = GameObject.FindGameObjectWithTag("SideNav");
+
+        for (int i = 0; i < demoList.Length; i++) {
+            Vector3 position = buttonOffset + new Vector3(0, -i * buttonPadding, 0);
+            Debug.Log(position);
+            createLevelButton(demoList[i], sideNav.transform, position);
+        }
+    }
+
+    private void selectDemo(DemoInfo info) {
+
+        TextMeshProUGUI infoTitle = GameObject.FindGameObjectWithTag("InfoTitle").GetComponentInChildren<TextMeshProUGUI>();
+        TextMeshProUGUI infoText = GameObject.FindGameObjectWithTag("InfoText").GetComponentInChildren<TextMeshProUGUI>();
+        Image infoImage = GameObject.FindGameObjectWithTag("InfoImage").GetComponent<Image>();
+       
+        infoTitle.text = info.demoTitle;
+        infoText.text = info.demoText;
+        infoImage.sprite = info.demoImage;
+
+        //string sceneName = info.sceneName;
+        //SceneManager.LoadScene(sceneName);
+    }
+
+    private GameObject createLevelButton(DemoInfo info, Transform parent, Vector3 position) {
+        GameObject buttonObj = Instantiate(selectButtonPrefab);
+        buttonObj.transform.parent = parent;
+        buttonObj.GetComponent<RectTransform>().anchoredPosition3D = position;
+        buttonObj.name = info.name + " Button";
+
+        TMPro.TextMeshProUGUI text = buttonObj.GetComponentInChildren<TMPro.TextMeshProUGUI>();
+        text.text = info.name;
+
+        Button button = buttonObj.GetComponent<Button>();
+        button.onClick.AddListener( delegate { selectDemo(info); } );
+        return buttonObj;
+    }
+
+}
---
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/PopupUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupUI : MonoBehaviour {
+
+    private static PopupUI instance = null;
+    public static PopupUI Instance {
+        get {
+            if (instance == null)  instance = GameObject.FindObjectOfType<PopupUI>();
+            return instance;
+        }
+    }
+
+    public void Start()
+    {
+        Button button = GetComponentInChildren<Button>();
+        button.onClick.AddListener(toggleShow);
+    }
+
+    public void toggleShow() {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+}
