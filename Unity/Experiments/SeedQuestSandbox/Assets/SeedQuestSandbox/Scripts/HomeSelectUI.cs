fcfb8400121894f531b95fa81a2b93388b8d03f6
Michael Mendoza <m.mendoza711@gmail.com>
2019-01-30 09:02:25 -0700

    Sandbox: Added H for shortcut for going to home level select scene for HomeSelectUI.

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/HomeSelectUI.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/HomeSelectUI.cs
@@ -7,6 +7,19 @@ public class HomeSelectUI : MonoBehaviour {
 
     public string sceneName = "SceneSelect";
 
+    private void Update()
+    {
+        ListenForKeyDown();
+    }
+
+    public void ListenForKeyDown()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
     public void GoToSceneSelect()
     {
         SceneManager.LoadScene(sceneName);
