ef907684fdd58c1acf4cb3b9a4dd38a0c4cebb0d
Brady Hammond <bradymh23@gmail.com>
2019-02-13 10:52:08 -0700

    Add Icon and Sound Effect

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/InteractableLockManager.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/InteractableLockManager.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class InteractableLockManager : MonoBehaviour {
 
+    public GameObject lockIcon;
     private bool locked = false;
+    private AudioSource click;
 
     void OnTriggerEnter(Collider enterObject)
     {
+        lockIcon.SetActive(true);
+        click = enterObject.GetComponent<AudioSource>();
+        click.Play();
         locked = true;
         FirstPersonCamera.SetFreeze = locked;
     }
@@ -21,6 +26,8 @@ public class InteractableLockManager : MonoBehaviour {
     {
         if (locked && Input.GetKeyDown(KeyCode.U))
         {
+            lockIcon.SetActive(false);
+            click.Play();
             locked = false;
             FirstPersonCamera.SetFreeze = locked;
         }
