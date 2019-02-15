98416f0dd34b05554cb62dccc81d95f96f0960d5
Weston Bodily <westonbodily7z@gmail.com>
2019-02-15 15:21:34 -0700

    Console command front end prototype built in Demon-tests scene

---
+++ Unity/SeedQuest/Assets/SeedQuest/Scripts/GameUI/ConsoleCommands/ConsoleCommandInput.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleCommandInput : MonoBehaviour {
+
+    public GameObject terminal;
+    public InputField inputField;
+
+    public Dictionary<string, System.Action> commands;
+
+    void Start () 
+    {
+        terminal = GetComponentInChildren<Terminal>(true).gameObject;
+        inputField = terminal.GetComponentInChildren<InputField>();
+	}
+	
+	void Update () 
+    {
+        
+        if (Input.GetKeyDown(KeyCode.BackQuote))
+        {
+            terminalToggleActive();
+        }
+        if(terminal.activeSelf && Input.GetKeyDown(KeyCode.Return))
+        {
+            parseInputCommand(inputField.text);
+            terminalToggleActive();
+        }
+
+	}
+
+    public void terminalToggleActive()
+    {
+        if (terminal.activeSelf)
+            terminal.SetActive(false);
+        else
+        {
+            terminal.SetActive(true);
+            inputField.Select();
+        }
+    }
+
+    public void parseInputCommand(string text)
+    {
+        Debug.Log("Input command recieved: " + text);
+    }
+
+
+
+}
