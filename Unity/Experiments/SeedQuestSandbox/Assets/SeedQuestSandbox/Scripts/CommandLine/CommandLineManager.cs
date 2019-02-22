d660a2b3541f7a0ba6135f73a2b95c75b0debb35
Weston Bodily <westonbodily7z@gmail.com>
2019-02-22 11:02:46 -0700

    Update to help function - now only displays available commands, does not display info on other topics anymore

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/CommandLine/CommandLineManager.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/CommandLine/CommandLineManager.cs
@@ -15,28 +15,13 @@ public static class CommandLineManager
         {"gamestate", setGameState}
     };
 
-    // Dictionary containing information on various topics for the user
-    public static Dictionary<string, string> helpInformation = new Dictionary<string, string>
-    {
-        {"seedquest", "SeedQuest is a mnemonic application to help a user recover a private key"},
-        {"rehearse", "Rehearsal mode is a practice mode to help a user remember the mnemonic path to recover their private key. Also called Learn mode"},
-        {"learn", "Learn mode is a practice mode to help a user remember the mnemonic path to recover their private key. Also called rehearsal mode"},
-        {"recall", "Recall mode is the mode used to recover a private key. Also called Recover mode"},
-        {"recover", "Recover mode is the mode used to recover a private key. Also called Recall mode"},
-
-    };
-
     // Displays some information to the user. If parameter string isn't found in helpInformation,
     //  then prints out a list of available commands
     public static string help(string input)
     {
-        if (helpInformation.ContainsKey(input))
-            return helpInformation[input];
-        else
-        {
-            privatePrint("Available command line commands: ");
-            foreach (string key in commands.Keys)
-                privatePrint(key);
+        privatePrint("Available command line commands: ");
+        foreach (string key in commands.Keys){
+            privatePrint(key);
         }
         return "";
     }
