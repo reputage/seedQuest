a1a81882741f8e1abe31663f5ccdf467d8f82fcb
Michael Mendoza <m.mendoza711@gmail.com>
2019-03-14 13:47:19 -0600

    Sandbox: Updated InteractablePreview with PreviewCube and fix HUD Manager

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/HUD/HUDManager.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/HUD/HUDManager.cs
@@ -27,52 +27,57 @@ public class HUDManager : MonoBehaviour {
     public void Awake()
     {
         GenerateHUD();
-        DestroyHUD();
+        DestroyImmediateHUD();
     }
 
     public void GenerateHUD() {
-        if(useCursor.use && useCursor.prefab != null && Resources.FindObjectsOfTypeAll<CursorUI>()[0] == null)
+        var test2 = GetComponentInChildren<CursorUI>();
+        var test4 = GetComponentInChildren<CursorUI>() == null;
+        var test3 = useCursor.prefab != null;
+        var test = useCursor.use && useCursor.prefab != null && GetComponentInChildren<CursorUI>(true) == null;
+
+        if(useCursor.use && useCursor.prefab != null && GetComponentInChildren<CursorUI>(true) == null)
             Instantiate(useCursor.prefab, transform);
-        if (useInteractablePreview.use && useInteractablePreview.prefab != null && Resources.FindObjectsOfTypeAll<InteractablePreviewUI>()[0] == null)
+        if (useInteractablePreview.use && useInteractablePreview.prefab != null && GetComponentInChildren<InteractablePreviewUI>(true) == null)
             Instantiate(useInteractablePreview.prefab, transform);
-        if (useHomeSelect.use && useHomeSelect.prefab != null && Resources.FindObjectsOfTypeAll<HomeSelectUI>()[0] == null)
+        if (useHomeSelect.use && useHomeSelect.prefab != null && GetComponentInChildren<HomeSelectUI>(true) == null)
             Instantiate(useHomeSelect.prefab, transform);
-        if (useEndGame.use && useEndGame.prefab != null && Resources.FindObjectsOfTypeAll<EndGameUI>()[0] == null)
+        if (useEndGame.use && useEndGame.prefab != null && GetComponentInChildren<EndGameUI>(true) == null)
             Instantiate(useEndGame.prefab, transform);
-        if (useLevelClear.use && useLevelClear.prefab != null && Resources.FindObjectsOfTypeAll<LevelClearUI>()[0] == null)
+        if (useLevelClear.use && useLevelClear.prefab != null && GetComponentInChildren<LevelClearUI>(true) == null)
             Instantiate(useLevelClear.prefab, transform);
-        if (useLevelName.use && useLevelName.prefab != null && Resources.FindObjectsOfTypeAll<LevelNameUI>()[0] == null)
+        if (useLevelName.use && useLevelName.prefab != null && GetComponentInChildren<LevelNameUI>(true) == null)
             Instantiate(useLevelName.prefab, transform);
-        if (useProgressTracker.use && useProgressTracker.prefab != null && Resources.FindObjectsOfTypeAll<ProgressTrackerUI>()[0] == null)
+        if (useProgressTracker.use && useProgressTracker.prefab != null && GetComponentInChildren<ProgressTrackerUI>(true) == null)
             Instantiate(useProgressTracker.prefab, transform);
-        if (useCLI.use && useCLI.prefab != null && Resources.FindObjectsOfTypeAll<CommandLineInputUI>()[0] == null)
+        if (useCLI.use && useCLI.prefab != null && GetComponentInChildren<CommandLineInputUI>(true) == null)
             Instantiate(useCLI.prefab, transform);
-        if (useTutorial.use && useTutorial.prefab != null && Resources.FindObjectsOfTypeAll<TutorialManager>()[0] == null)
+        if (useTutorial.use && useTutorial.prefab != null && GetComponentInChildren<TutorialManager>(true) == null)
             Instantiate(useTutorial.prefab, transform);
-        if (useInteractableTracker.use && useInteractableTracker.prefab != null && Resources.FindObjectsOfTypeAll<InteractableTrackerUI>()[0] == null)
+        if (useInteractableTracker.use && useInteractableTracker.prefab != null && GetComponentInChildren<InteractableTrackerUI>(true) == null)
             Instantiate(useInteractableTracker.prefab, transform);
     }
 
-    public void DestroyHUD() {
-        if (!useCursor.use && Resources.FindObjectsOfTypeAll<CursorUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<CursorUI>()[0].gameObject);
-        if (!useInteractablePreview.use && Resources.FindObjectsOfTypeAll<InteractablePreviewUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<InteractablePreviewUI>()[0].gameObject);
-        if (!useHomeSelect.use && Resources.FindObjectsOfTypeAll<HomeSelectUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<HomeSelectUI>()[0].gameObject);
-        if (!useEndGame.use && Resources.FindObjectsOfTypeAll<EndGameUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<EndGameUI>()[0].gameObject);
-        if (!useLevelClear.use && Resources.FindObjectsOfTypeAll<LevelClearUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<LevelClearUI>()[0].gameObject);
-        if (!useLevelName.use && Resources.FindObjectsOfTypeAll<LevelNameUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<LevelNameUI>()[0].gameObject);
-        if (!useProgressTracker.use && Resources.FindObjectsOfTypeAll<ProgressTrackerUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<ProgressTrackerUI>()[0].gameObject);
-        if (!useCLI.use && Resources.FindObjectsOfTypeAll<CommandLineInputUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<CommandLineInputUI>()[0].gameObject);
-        if (!useTutorial.use && Resources.FindObjectsOfTypeAll<TutorialManager>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<TutorialManager>()[0].gameObject);
-        if (!useInteractableTracker.use && Resources.FindObjectsOfTypeAll<InteractableTrackerUI>()[0] != null)
-            Destroy(Resources.FindObjectsOfTypeAll<InteractableTrackerUI>()[0].gameObject);
+    public void DestroyImmediateHUD() {
+        if (!useCursor.use && GetComponentInChildren<CursorUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<CursorUI>(true).gameObject);
+        if (!useInteractablePreview.use && GetComponentInChildren<InteractablePreviewUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<InteractablePreviewUI>(true).gameObject);
+        if (!useHomeSelect.use && GetComponentInChildren<HomeSelectUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<HomeSelectUI>(true).gameObject);
+        if (!useEndGame.use && GetComponentInChildren<EndGameUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<EndGameUI>(true).gameObject);
+        if (!useLevelClear.use && GetComponentInChildren<LevelClearUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<LevelClearUI>(true).gameObject);
+        if (!useLevelName.use && GetComponentInChildren<LevelNameUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<LevelNameUI>(true).gameObject);
+        if (!useProgressTracker.use && GetComponentInChildren<ProgressTrackerUI>() != null)
+            DestroyImmediate(GetComponentInChildren<ProgressTrackerUI>(true).gameObject);
+        if (!useCLI.use && GetComponentInChildren<CommandLineInputUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<CommandLineInputUI>(true).gameObject);
+        if (!useTutorial.use && GetComponentInChildren<TutorialManager>(true) != null)
+            DestroyImmediate(GetComponentInChildren<TutorialManager>(true).gameObject);
+        if (!useInteractableTracker.use && GetComponentInChildren<InteractableTrackerUI>(true) != null)
+            DestroyImmediate(GetComponentInChildren<InteractableTrackerUI>(true).gameObject);
     }
 }
