d49a9edb5d06e7517edba713f1a54c2311a7a212
Michael Mendoza <m.mendoza711@gmail.com>
2019-03-19 00:41:22 -0600

    Sandbox: Added customizablity to CardPopupUI

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/General UI/CardPopupUI.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/General UI/CardPopupUI.cs
@@ -6,28 +6,49 @@ using TMPro;
 
 public class CardPopupUI : MonoBehaviour
 {
-    public Color32 primaryColor = new Color32(0x21, 0x96, 0xf3, 0xff);
-    public Color32 backgroundColor = new Color32(0xff, 0xff, 0xff, 0xff);
+    public Color32 headerColor = new Color32(0x21, 0x96, 0xf3, 0xff);
+    public Color32 headerTextColor = new Color32(0xff, 0xff, 0xff, 0xff);
+    public Color32 cardColor = new Color32(0xff, 0xff, 0xff, 0xff);
+    public Color32 cardTextColor = new Color32(0x00, 0x00, 0x00, 0xff);
     public Color backdropColor = new Color(0, 0, 0, 0.4f);
 
-    private Image[] images;
+    public string headerText = "Title";
+    public string cardText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ";
 
-    public void Start()
-    {
+    public bool useButtonOne = true;
+    public string buttonOneText = "Accept";
+    public bool useButtonTwo = true;
+    public string buttonTwoText = "Cancel";
+
+    private Image[] images;
+    private TextMeshProUGUI[] texts;
+    private Button[] buttons;
+
+    public void Start() {
         gameObject.SetActive(false);         // Default popup inactive
 
         images = GetComponentsInChildren<Image>();
+        texts = GetComponentsInChildren<TextMeshProUGUI>();
+        buttons = GetComponentsInChildren<Button>();
     }
 
-    public void Update()
-    {
+    public void Update() {
         images[0].color = backdropColor;
-        images[1].color = backgroundColor;
-        images[2].color = primaryColor;
+        images[1].color = cardColor;
+        images[2].color = headerColor;
+
+        texts[0].text = headerText;
+        texts[1].color = headerTextColor;
+        texts[1].text = cardText;
+        texts[1].color = cardTextColor;
+        texts[2].text = buttonOneText;
+        texts[3].text = buttonTwoText;
+
+        buttons[1].gameObject.SetActive(useButtonOne);
+        buttons[2].gameObject.SetActive(useButtonTwo);
     }
 
-    public void toggleShow()
-    {
+    public void toggleShow() {
         gameObject.SetActive(!gameObject.activeSelf);
     }
 }
