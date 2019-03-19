1051af0acc37d1fa225b8e20d300563011cc4b1d
Michael Mendoza <m.mendoza711@gmail.com>
2019-03-19 00:18:43 -0600

    Updates to CardPopupUI

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/General UI/CardPopupUI.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/General UI/CardPopupUI.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class CardPopupUI : MonoBehaviour 
+public class CardPopupUI : MonoBehaviour
 {
-    public void Start() {
+    public Color32 primaryColor = new Color32(0x21, 0x96, 0xf3, 0xff);
+    public Color32 backgroundColor = new Color32(0xff, 0xff, 0xff, 0xff);
+    public Color backdropColor = new Color(0, 0, 0, 0.4f);
+
+    private Image[] images;
+
+    public void Start()
+    {
         gameObject.SetActive(false);         // Default popup inactive
+
+        images = GetComponentsInChildren<Image>();
     }
 
-    public void toggleShow() {
+    public void Update()
+    {
+        images[0].color = backdropColor;
+        images[1].color = backgroundColor;
+        images[2].color = primaryColor;
+    }
+
+    public void toggleShow()
+    {
         gameObject.SetActive(!gameObject.activeSelf);
     }
 }
