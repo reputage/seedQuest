de9b96e96ee014fb71b1e82640bedb41d7718173
Michael Mendoza <m.mendoza711@gmail.com>
2019-03-29 18:10:57 -0600

    Sandbox: Updated EndGameUI text

--- Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/HUD/EndGameUI.cs
+++ Unity/Experiments/SeedQuestSandbox/Assets/SeedQuestSandbox/Scripts/HUD/EndGameUI.cs
@@ -25,7 +25,7 @@ public class EndGameUI : MonoBehaviour {
         textList[0].text = converter.DecodeSeed();
 
         if (GameManager.Mode == GameMode.Rehearsal)
-            textList[1].text = "Rehearsal Complete! \n Need more practice? Select Learn mode. \n Ready to go? Select Recover mode";
+            textList[1].text = "Rehearsal Complete! \n Need more practice? Select 'Learn'. \n Ready to go? Select 'Recover";
         else
             textList[1].text = "Key is Recovered!";
     }
